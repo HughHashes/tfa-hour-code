424dbb8d919472e3ed7157dcdcfec081d9509a29
Louis CarricoBraun <lcarrico-braun@wlapcs.org>
2017-03-09 23:55:06 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup(){
+    createCanvas(600,400);
+    background(42, 229, 213);
+    
+    
+}
+
+function draw(){
+    fill(100, 100, 255);
+    noStroke();
+    ellipse(mouseX, mouseY, 75, 75);
+    
+    
+    
+}
